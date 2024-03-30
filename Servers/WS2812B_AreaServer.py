62790fd4cf8aff0be91ed200403e7aee3df4107f
Home <wjdals7863@naver.com>
2024-03-30 10:49:14 +0900

    WS2812B.py: Preparing to generate code

--- Servers/WS2812B_AreaServer.py
+++ Servers/WS2812B_AreaServer.py
@@ -1,5 +1,15 @@
 import cv2, time
 
+'''
+WS2812B LED 데이터시트 https://pdf1.alldatasheet.co.kr/datasheet-pdf/view/1134589/WORLDSEMI/WS2818A.html
+
+X : 0 신호 함수 작성
+X : 1 신호 함수 작성
+X : RGB 24bit 생성 함수 작성
+X : LED 갯수별 RGB 24bit 세트 생성 함수 생성
+X : RGB data 쓰기 함수 작성
+
+'''
 
 class WS2812B_AreaServer:
     def __init__(self):
